68ccea08c9c9c3aca77cc284c21e3bf5dab182f1
Anjesh18 <133150349+Anjesh18@users.noreply.github.com>
2024-09-16 12:28:58 +0530

    Delete routes directory

--- routes/CompanyRoute.js
+++
@@ -1,9 +0,0 @@
-import { getCompany, getCompanyById, registerCompany, updateCompany } from "../controllers/companyController.js";
-import express from 'express'
-import { isAuthenticated } from "../middlewares/Auth.js";
-export const companyRouter=express.Router()
-
-companyRouter.post('/register', isAuthenticated,registerCompany)
-companyRouter.get('/get',isAuthenticated, getCompany)
-companyRouter.get('/get/:id',isAuthenticated, getCompanyById) 
-companyRouter.put('/update/:id',isAuthenticated, updateCompany)
--- routes/jobRouter.js
+++
@@ -1,9 +0,0 @@
-import { postJob, getJob, getJobById,getAdminJobs } from "../controllers/jobController.js";
-import { isAuthenticated } from "../middlewares/Auth.js";
-import express from 'express'
-export const jobRouter=express.Router()
-
-jobRouter.post('/post',isAuthenticated, postJob)
-jobRouter.get('/get',isAuthenticated, getJob)
-jobRouter.get('/get/:id',isAuthenticated, getJobById)
-jobRouter.get('/getadminjobs',isAuthenticated, getAdminJobs)
